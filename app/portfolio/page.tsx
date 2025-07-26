3b8b6546730fa90f27cf1ec3a4fa02553034d60b
cybexonics <cybexonicsitconsultants@gmail.com>
2025-07-26 13:40:33 +0530

    replace_images

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -48,7 +48,7 @@ export default function PortfolioPage() {
       id: 1,
       category: "Realism",
       type: "image",
-      media: "/hand3.jpg?height=900&width=600&text=Realistic+Tattoo",
+      media: "/hand8.JPG?height=900&width=600&text=Realistic+Tattoo",
     },
     {
       id: 2,
